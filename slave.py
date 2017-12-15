dcc71ef431f414cbbeaefbcdf54177e11f3d658a
shenoyv <shenoyv@tcd.ie>
2017-12-15 20:58:57 +0000

    new lines to go to depth of the tree

--- slave.py
+++ slave.py
@@ -14,3 +14,15 @@ def compute_complexity(source) #function to calculate codecomplexity
     for func in blocks:
         result.append(func.name+"- CC Rank:"+cc_rank(func.complexity))
     return result
+
+def tot_data(tree, repo):
+      datas = []
+    for x in tree:
+        if ".py" in x.name:
+             datas.append(x)
+        if "." not in x.name:
+           if x.type == 'tree':
+                new_tree = repo.get(x.id)
+                datas += (tot_data(new_tree, repo))
+    return datas
+
